b1f9b4ac98dc8acad8396d1dbf960cf8cb837219
Will Fisher <wfisher@chef.io>
2019-06-08 17:22:55 -0700

    Adding logger tests
    Signed-off-by: Will Fisher <wfisher@chef.io>

---
+++ spec/logger_spec.rb
@@ -0,0 +1,28 @@
+require 'automate_api'
+
+describe AutomateApi::Logger do
+  context 'default' do
+    it 'should not print debug' do
+      expect { AutomateApi::Logger.debug('test') }.to_not output(/test/).to_stdout
+    end
+
+    it 'should print info' do
+      expect { AutomateApi::Logger.info('something or other') }.to output(/INFO/).to_stdout
+    end
+
+    it 'should print warn' do
+      expect { AutomateApi::Logger.warn('warning!') }.to output(/WARN/).to_stdout
+    end
+
+    it 'should print formatted output' do
+      expect { AutomateApi::Logger.display('type', 'message') }.to output(/\[TYPE\] message/).to_stdout
+    end
+  end
+
+  context 'with debug enabled' do
+    it 'should print debug' do
+      AutomateApi::Config.debug = true
+      expect { AutomateApi::Logger.debug('test') }.to output(/test/).to_stdout
+    end
+  end
+end
